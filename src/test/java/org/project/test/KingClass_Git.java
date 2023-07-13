530553471e3067615af33c7c3f81d72c28dc8a67
LeninGT <g.t.lenin37@gmail.com>
2023-07-13 16:19:32 +0530

    kings new code

--- src/test/java/org/project/test/KingClass_Git.java
+++ src/test/java/org/project/test/KingClass_Git.java
@@ -5,6 +5,9 @@ public class KingClass_Git {
 	public static void main(String[] args) {
 		
 		System.out.println("SidClass code");
+		System.out.println("VIKRAM");
+		System.out.println("kamal");
+		System.out.println("dhanush");
 	}
 
 }
